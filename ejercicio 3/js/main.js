a4ddc14f9d2a9a8cb65edfd7fcb14c02480d25aa
Camilo Ignacio Zavala Cornejo <49605611+C1ZC@users.noreply.github.com>
2022-09-26 03:06:45 -0300

    Add files via upload

---
+++ ejercicio 3/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 150
+    }
+        );
+  });
